26e0fa2ec0950d114556129c1c45d20125d634c4
Dr. D CODER <dhruviklathiya9999@gmail.com>
2024-08-04 20:51:52 +0530

    Assignment 1

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,22 @@
-console.log("Hello world")
+// Assignment 1: Sum function
+const calculate_sum = (_para1,_para2) => {
+    return _para1 + _para2
+}
+
+console.log(calculate_sum(10,20))
+
+// Same task with string as parameter
+const calculate_sum_string = (_para1,_para2) => {
+    return _para1 + _para2
+}
+
+console.log(calculate_sum_string("10","20"))
+// Polymorphism is the answer for weird nature of + operation on string
+    // + operator works as concatenator for strings and Arithmetic operator for numbers
+
+// Same task with string as parameter but number as output
+const calculate_sum_from_string = (_para1,_para2) => {
+    return Number(_para1) + Number(_para2)
+}
+
+console.log(calculate_sum_from_string("10","20"))
